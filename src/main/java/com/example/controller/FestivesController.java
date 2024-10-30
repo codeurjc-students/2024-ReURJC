10272424e915bce4c6d3560ab5fb5cd29f99336f
jesussmariscal <mariscalalonso16@icloud.com>
2024-10-30 08:16:49 +0100

    bug fixed

--- src/main/java/com/example/controller/FestivesController.java
+++ src/main/java/com/example/controller/FestivesController.java
@@ -1,9 +1,12 @@
 package com.example.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.model.Festive;
 import com.example.services.FestiveService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,7 +21,7 @@ public class FestivesController {
 
 
     @GetMapping("/api/festives")
-    public ResponseEntity<?> getMethodName() {
+    public ResponseEntity<List<Festive>> getMethodName() {
         return ResponseEntity.ok(service.getAll());
     }
     
