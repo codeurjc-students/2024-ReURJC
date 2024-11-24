558e7ee37158d6f55a88f1b89241f551d9410975
jesussmariscal <mariscalalonso16@icloud.com>
2024-11-24 19:22:32 +0100

    notification dot hidden after cheking the notification pannel

--- MyURJC/src/app/home/home.page.ts
+++ MyURJC/src/app/home/home.page.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { IonContent } from '@ionic/angular';
+import { IonContent, IonTab, IonTabs } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage{
+export class HomePage {
+onTabChange(event: any) {
+  if (this.previousTab === 'alerts' && event.tab !== 'alerts') {
+    // Se acaba de salir de la pestaña "alerts"
+    this.showNotificationBadge = false; 
+  }
+  this.previousTab = event.tab; 
+}
   
   showNotificationBadge = false
+  previousTab: string | undefined;
 
 
 
