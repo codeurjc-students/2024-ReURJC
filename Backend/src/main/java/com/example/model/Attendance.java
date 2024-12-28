a0ea15ce36e24b60e9e4cce3b8c410dee2c49ac5
jesussmariscal <mariscalalonso16@icloud.com>
2024-12-28 14:00:10 +0100

    cleaned imports.

--- Backend/src/main/java/com/example/model/Attendance.java
+++ Backend/src/main/java/com/example/model/Attendance.java
@@ -1,9 +1,5 @@
 package com.example.model;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
-import com.example.services.AttendanceService;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
