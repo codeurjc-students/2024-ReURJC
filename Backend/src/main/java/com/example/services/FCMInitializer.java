c8668bd2b7974bb5d908aa3873fcd2138db0cd61
jesussmariscal <mariscalalonso16@icloud.com>
2024-12-28 13:00:21 +0100

    changes in FCM

--- Backend/src/main/java/com/example/services/FCMInitializer.java
+++ Backend/src/main/java/com/example/services/FCMInitializer.java
@@ -11,46 +11,27 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
-import java.io.InputStream;
 
 @Service
 public class FCMInitializer {
 
+    @Value("${app.firebase-configuration-file}")
+    private String firebaseConfigPath;
     Logger logger = LoggerFactory.getLogger(FCMInitializer.class);
-
-    @Value("${firebase.service-account-file}")
-    private String serviceAccountFile = "";
 
     @PostConstruct
     public void initialize() {
         try {
-            InputStream serviceAccount;
-
-            // Comprueba si se está ejecutando en Docker
-            if (isRunningInDocker()) {
-                logger.info("Detected Docker environment.");
-                serviceAccount = new ClassPathResource(serviceAccountFile).getInputStream();
-                logger.info("Firebase Admin SDK initialized using service account file in Docker: " + serviceAccountFile);
-            } else {
-                // Si no está en Docker, intentamos con el classpath
-                serviceAccount = new ClassPathResource(serviceAccountFile).getInputStream();
-                logger.info("Firebase Admin SDK initialized using classpath service account file: " + serviceAccountFile);
-            }
-
-            /*FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream("hola"))
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(
+                            GoogleCredentials.fromStream(new ClassPathResource(firebaseConfigPath).getInputStream()))
                     .build();
-
             if (FirebaseApp.getApps().isEmpty()) {
                 FirebaseApp.initializeApp(options);
                 logger.info("Firebase application initialized");
-            }*/
+            }
         } catch (IOException e) {
-            logger.error("Error initializing Firebase", e);
+            logger.error(e.getMessage());
         }
-    }
-    
-    private boolean isRunningInDocker() {
-        return "true".equals(System.getenv("RUNNING_IN_DOCKER"));
     }
 }
