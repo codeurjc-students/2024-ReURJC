b3c61d79bcc710b1f9a9f6bcaa69ed1aa92d76d7
jesussmariscal <mariscalalonso16@icloud.com>
2024-12-28 12:08:41 +0100

    Workflow optimized

--- Backend/src/main/java/com/example/services/FCMInitializer.java
+++ Backend/src/main/java/com/example/services/FCMInitializer.java
@@ -5,47 +5,52 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 
 @Service
 public class FCMInitializer {
 
     Logger logger = LoggerFactory.getLogger(FCMInitializer.class);
 
+    @Value("${firebase.service-account-file}")
+    private String serviceAccountFile;
+
     @PostConstruct
     public void initialize() {
-      String firebaseCredentials = null;
-      try {
-        firebaseCredentials = System.getenv("FCM_PRIVATE_KEY");
-        InputStream serviceAccount;
-        if (firebaseCredentials != null && !firebaseCredentials.isEmpty()) {
-          // Si la variable de entorno está presente, usarla
-          serviceAccount = new ByteArrayInputStream(firebaseCredentials.getBytes(StandardCharsets.UTF_8));
-          logger.info("Firebase Admin SDK initialized using environment variable.");
-        } else {
-          // Si no, intentar cargar desde el archivo (embebido en la imagen)
-          
-          serviceAccount = new FileInputStream("/app/firebase-service-account.json");
-          logger.warn("Firebase Admin SDK initialized using embedded service account file. This is not recommended for production.");
-        }
+        try {
+            InputStream serviceAccount;
 
-        FirebaseOptions options = new FirebaseOptions.Builder()
-            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-            .build();
+            // Comprueba si se está ejecutando en Docker
+            if (isRunningInDocker()) {
+                logger.info("Detected Docker environment.");
+                serviceAccount = new ClassPathResource(serviceAccountFile).getInputStream();
+                logger.info("Firebase Admin SDK initialized using service account file in Docker: " + serviceAccountFile);
+            } else {
+                // Si no está en Docker, intentamos con el classpath
+                serviceAccount = new ClassPathResource(serviceAccountFile).getInputStream();
+                logger.info("Firebase Admin SDK initialized using classpath service account file: " + serviceAccountFile);
+            }
 
-        if (FirebaseApp.getApps().isEmpty()) {
-          FirebaseApp.initializeApp(options);
-          logger.info("Firebase application initialized");
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+                logger.info("Firebase application initialized");
+            }
+        } catch (IOException e) {
+            logger.error("Error initializing Firebase", e);
         }
-      } catch (IOException e) {
-        logger.error("Error initializing Firebase", e);
-      }
+    }
+    
+    private boolean isRunningInDocker() {
+        return "true".equals(System.getenv("RUNNING_IN_DOCKER"));
     }
 }
